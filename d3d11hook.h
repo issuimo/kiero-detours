101c07a93d5bcc31f44fd61431dc00dfe70d9882
遂沫 <1992724048@qq.com>
2023-08-25 11:15:03 +0800

    new

--- d3d11hook.h
+++ d3d11hook.h
@@ -5,15 +5,15 @@
 
 #pragma comment(lib, "d3d11.lib")
  
-namespace dxhook {
+namespace DxHook {
 	class Hk11 {
 		using IDXGISwapChainPresent = HRESULT(*)(IDXGISwapChain*, UINT, UINT);
 	public:
-		static auto Build(std::function<void()> fn) -> bool {
+		static auto Build(const std::function<void()>& fn) -> bool {
             if (!fn)
                 return false;
 
-            if (IDXGISwapChainPresent pre = findDirect11Present()) {
+            if (const IDXGISwapChainPresent pre = findDirect11Present()) {
                 present = fn;
                 return HookManager::install(pre, MyPresent);
             }
@@ -23,25 +23,23 @@ namespace dxhook {
 
 		static auto SetWndProc(char(*wndProc)(HWND, UINT, WPARAM, LPARAM)) -> void {
             WndProc = wndProc;
-            oldWndProc = reinterpret_cast<WNDPROC>(GetWindowLongPtr(hwnd, GWLP_WNDPROC));
-            SetWindowLongPtr(hwnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(NewWndProc));
+            oldWndProc = reinterpret_cast<WNDPROC>(GetWindowLongPtr(hWnd, GWLP_WNDPROC));
+            SetWindowLongPtr(hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(NewWndProc));
 		}
 
 		static auto Unbuild() -> void { HookManager::detach(MyPresent); }
 
-		inline static auto GetHwnd() -> HWND { return hwnd; }
+		inline static auto GetHwnd() -> HWND { return hWnd; }
 		inline static auto GetDevice() -> ID3D11Device* { return g_Device; }
 		inline static auto GetSwapChain() -> IDXGISwapChain* { return g_SwapChain; }
 		inline static auto GetContext() -> ID3D11DeviceContext* { return g_Context; }
 		inline static auto GetTargetView() -> ID3D11RenderTargetView* const * { return &g_TargetView; }
 
 	private:
-		inline static bool inited{false};
+		inline static bool Init{false};
 
-		inline static HWND hwnd{};
+		inline static HWND hWnd{};
 		inline static WNDPROC oldWndProc{};
-        inline static UINT g_ResizeWidth;
-        inline static UINT g_ResizeHeight;
 
 		inline static std::function<void()> present;
 		inline static char(*WndProc)(HWND, UINT, WPARAM, LPARAM);
@@ -60,7 +58,7 @@ namespace dxhook {
             if (!RegisterClassEx(&wc))
                 return nullptr;
 
-            HWND hWnd = CreateWindow(wc.lpszClassName, TEXT(""), WS_DISABLED, 0, 0, 0, 0, NULL, NULL, NULL, nullptr);
+			const HWND hWnd = CreateWindow(wc.lpszClassName, TEXT(""), WS_DISABLED, 0, 0, 0, 0, NULL, NULL, NULL, nullptr);
             IDXGISwapChain* pSwapChain;
 
             D3D_FEATURE_LEVEL    featureLevel;
@@ -92,7 +90,7 @@ namespace dxhook {
             const DWORD_PTR* pSwapChainVtable = reinterpret_cast<DWORD_PTR*>(pSwapChain);
             pSwapChainVtable = reinterpret_cast<DWORD_PTR*>(pSwapChainVtable[0]);
 
-            auto swapChainPresent = reinterpret_cast<IDXGISwapChainPresent>(pSwapChainVtable[8]);
+			const auto swapChainPresent = reinterpret_cast<IDXGISwapChainPresent>(pSwapChainVtable[8]);
 
             pDevice->Release();
             pContext->Release();
@@ -104,31 +102,20 @@ namespace dxhook {
 		}
 
 		static auto MyPresent(IDXGISwapChain* a, UINT b, UINT c) -> HRESULT {
-            if (!inited) {
-                g_SwapChain = a;
-                auto result = a->GetDevice(__uuidof(g_Device), reinterpret_cast<void**>(&g_Device));
+            if (!Init) {
+                g_SwapChain       = a;
+				const auto result = a->GetDevice(__uuidof(g_Device), reinterpret_cast<void**>(&g_Device));
                 if (SUCCEEDED(result)) {
                     g_Device->GetImmediateContext(&g_Context);
                     DXGI_SWAP_CHAIN_DESC sd;
                     a->GetDesc(&sd);
-                    hwnd = sd.OutputWindow;
+                    hWnd = sd.OutputWindow;
                     ID3D11Texture2D* pBackBuffer{};
                     a->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<LPVOID*>(&pBackBuffer));
                     g_Device->CreateRenderTargetView(pBackBuffer, nullptr, &g_TargetView);
                     pBackBuffer->Release();
-                    inited = true;
+                    Init = true;
                 }
-            }
-
-            // Handle window resize (we don't resize directly in the WM_SIZE handler)
-            if (g_ResizeWidth != 0 && g_ResizeHeight != 0) {
-                g_TargetView->Release();
-                g_SwapChain->ResizeBuffers(0, g_ResizeWidth, g_ResizeHeight, DXGI_FORMAT_UNKNOWN, 0);
-                g_ResizeWidth = g_ResizeHeight = 0;
-                ID3D11Texture2D* pBackBuffer;
-                g_SwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
-                g_Device->CreateRenderTargetView(pBackBuffer, nullptr, &g_TargetView);
-                pBackBuffer->Release();
             }
 
             present();
@@ -137,14 +124,6 @@ namespace dxhook {
 		}
 
 		static auto CALLBACK NewWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) -> LRESULT {
-			switch (uMsg) {
-	            case WM_SIZE:
-	                if (wParam == SIZE_MINIMIZED)
-	                    return 0;
-                    g_ResizeWidth  = static_cast<UINT>(LOWORD(lParam)); // Queue resize
-                    g_ResizeHeight = static_cast<UINT>(HIWORD(lParam));
-			}
-
 			const auto ret = WndProc(hWnd, uMsg, wParam, lParam);
             if (ret == 1)
                 return CallWindowProc(oldWndProc, hWnd, uMsg, wParam, lParam);
@@ -153,6 +132,7 @@ namespace dxhook {
             if (ret == 2) {
                 return NULL;
             }
+            return NULL;
 		}
 	};
 }
